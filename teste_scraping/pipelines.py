a992526695b6df405733ab1f0527f5672221f5bc
Eudess <jadieleudes@gmail.com>
2020-10-18 10:08:48 -0300

    ajuste na função que gera lista de dicionário de envolvidos

--- teste_scraping/pipelines.py
+++ teste_scraping/pipelines.py
@@ -18,8 +18,10 @@ class TesteScrapingPipeline:
             item['numero_processo'] = item['numero_legado']
 
         item['data_autuacao'] = self.cleaned_data_autuacao(item['data_autuacao'])
-        item['envolvidos'] = self.cleaned_envolvidos(self.cleaned_list(item['envolvidos']))
-        item['movimentacoes'] = self.cleaned_list((item['movimentacoes']))
+        item['envolvidos'] = self.cleaned_envolvidos(self.cleaned_char_envolvidos(item['envolvidos']))
+        item['movimentacoes'] = self.cleaned_char_movimentacoes((item['movimentacoes']))
+        #item['movimentacoes'] = self.cleaned_movimentacoes(self.cleaned_list((item['movimentacoes'])))
+
 
         return item
 
@@ -31,12 +33,12 @@ class TesteScrapingPipeline:
                 new_date += char
         return new_date
 
-    def cleaned_list(self, string_list):
+    def cleaned_char_envolvidos(self, envolvidos_list):
         new_list = []
         final_list = []
         new_string = ""
         string_check = ":\xa0\n\t"
-        for word in string_list:
+        for word in envolvidos_list:
             for char in word:
                 if char not in string_check:
                     new_string += char
@@ -49,18 +51,36 @@ class TesteScrapingPipeline:
         return final_list
 
 
-    def cleaned_envolvidos(self, string_list):
+    def cleaned_envolvidos(self, envolvidos_list):
         count1 = 0
         count2 = 1
         new_list = []
         check_string1 = "DESEMBARGADOR FEDERAL "
         check_string2 = "DESEMBARGADOR(A) FEDERAL "
-        while count2 < len(string_list):
-            if check_string1 in string_list[count2]:
-                string_list[count2] = string_list[count2].replace(check_string1, "")
-            if check_string2 in string_list[count2]:
-                string_list[count2] = string_list[count2].replace(check_string2, "")
-            new_list.append((string_list[count1], string_list[count2]))
+        while count2 < len(envolvidos_list):
+            if check_string1 in envolvidos_list[count2]:
+                envolvidos_list[count2] = envolvidos_list[count2].replace(check_string1, "")
+            if check_string2 in envolvidos_list[count2]:
+                envolvidos_list[count2] = envolvidos_list[count2].replace(check_string2, "")
+            new_list.append({envolvidos_list[count1]: envolvidos_list[count2]})
             count1 += 2
             count2 += 2
-        return dict(new_list)
+        return new_list
+
+    def cleaned_char_movimentacoes(self, movimentacoes_list):
+        new_list = []
+        final_list = []
+        new_string = ""
+        string_check = "\xa0\n\t"
+        for word in movimentacoes_list:
+            for char in word:
+                if char not in string_check:
+                    new_string += char
+            new_list.append(new_string)
+            new_string = ""
+
+        for word in new_list:
+            if word != "":
+                final_list.append(word.strip())
+        return final_list
+
